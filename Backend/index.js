7fa2080b99e138470e655edc3efeb070b51b5cce
SebastianSanchez22 <sebastiansaor22@gmail.com>
2022-10-25 01:24:35 -0500

    Integración bot, base de datos y API

--- Backend/index.js
+++ Backend/index.js
@@ -58,10 +58,79 @@ function initializeBot(){
     })
 
     bot.hears('Prueba', (ctx) => {
-        asesoria('Juan','3042812771','Whatsapp','Bicicletas','Juan Asesor',5187647590);
+        asesoria(5187647590,'Juan el asesor','Bicicletas','Whatsapp');
     })
     console.log("Ejecutando bot")
 }    
 
 initializeBot();
 
+function asesoria(chatid,asesorName,asesoradoName,phoneNumber,category,medio){
+
+    let unmensaje = 'Hola '+asesorName+', '+asesoradoName+' está solicitando una asesoria en el tema de '+category+' por medio de: '+medio+ ' , su número es: '+phoneNumber;
+    bot.telegram.sendMessage(chatid, unmensaje, {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                        text: "La tomo",
+                        callback_data: 'SI'
+                    },
+                    {
+                        text: "No Ahora",
+                        callback_data: 'NO'
+                    }
+                ],
+
+            ]
+        }
+    })
+}
+
+
+async function QueryAsesores(categoriaBuscada){
+    const [asesoresParaCategoria, metadata1] = await db.query(`SELECT x.telegram_id,x.nombreAsesor FROM asesor x INNER JOIN asesor_categoria y ON x.id_asesor=y.id_asesor WHERE y.id_categoria = (SELECT id_categoria FROM categoria WHERE categoria.nombreCategoria="${categoriaBuscada}")`)
+    return asesoresParaCategoria;
+    }
+
+async function buscarAsesor(){
+    const [asesoriasPendientes, metadata] = await db.query("SELECT * FROM asesoria WHERE estado = 'Pendiente'")
+    for (let i=0;i<asesoriasPendientes.length;i++){
+        let asesores = await QueryAsesores(asesoriasPendientes[i].categoria)
+        for (let j=0;j<asesores.length;j++){
+            asesoria(asesores[j].telegram_id,asesores[j].nombreAsesor,asesoriasPendientes[i].nombreAsesorado,asesoriasPendientes[i].celular, asesoriasPendientes[i].categoria,asesoriasPendientes[i].plataforma)
+        }
+    }
+}
+
+const buscarAsesorias = await buscarAsesor();
+
+buscarAsesorias;
+
+//const [asesoriasPendientes, metadata] = await db.query("SELECT * FROM asesoria WHERE estado = 'Pendiente'")
+//console.log(asesoriasPendientes)
+
+//const categoriaBuscada = "Televisores";
+//const [asesoresParaCategoria, metadata1] = await db.query(`SELECT x.telegram_id,x.nombreAsesor FROM asesor x INNER JOIN asesor_categoria y ON x.id_asesor=y.id_asesor WHERE y.id_categoria = (SELECT id_categoria FROM categoria WHERE categoria.nombreCategoria="${categoriaBuscada}")`);
+//console.log(asesoresParaCategoria);
+
+/*
+async function QueryAsesores(categoriaBuscada){
+const [asesoresParaCategoria, metadata1] = await db.query(`SELECT x.telegram_id,x.nombreAsesor FROM asesor x INNER JOIN asesor_categoria y ON x.id_asesor=y.id_asesor WHERE y.id_categoria = (SELECT id_categoria FROM categoria WHERE categoria.nombreCategoria="${categoriaBuscada}")`)
+return asesoresParaCategoria;
+}
+
+const asesoresDisponibles = await QueryAsesores("Televisores");
+console.log(asesoresDisponibles);
+*/
+
+//const categoriaBuscada = 'Vehiculos'; 
+//const [asesoresParaCategoria, metadata1] = await db.query(`SELECT x.telegram_id,x.nombreAsesor FROM asesor x INNER JOIN asesor_categoria y ON x.id_asesor=y.id_asesor WHERE y.id_categoria = (SELECT id_categoria FROM categoria WHERE categoria.nombreCategoria="${categoriaBuscada}")`);
+//console.log(asesoresParaCategoria);
+
+//const idABuscar = 5;
+//const [estadoAsesoria, metadata2] = await db.query(`SELECT estado FROM asesoria WHERE id_asesoria = ${idABuscar}`)
+//console.log(estadoAsesoria)
+
+
+//const actualizarEstado = await Asesoria.update({ estado : 'Tomada'}, {where : { estado : 'Pendiente'}})
+// const [actualizarEstado, metadata3] = await db.query("")
